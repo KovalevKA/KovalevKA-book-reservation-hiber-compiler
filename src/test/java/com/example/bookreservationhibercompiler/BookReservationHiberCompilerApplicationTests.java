4e73c27d196585082ed35befde5b1845c9cdf4f7
KovalevKA <kss47242@gmail.com>
2021-08-24 15:24:46 +0300

    first start

---
+++ src/test/java/com/example/bookreservationhibercompiler/BookReservationHiberCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookreservationhibercompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationHiberCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
