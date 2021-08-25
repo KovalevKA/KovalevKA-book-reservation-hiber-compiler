f4ffd54bc0a9455a612524453ccbdcc540577937
KovalevKA <kss47242@gmail.com>
2021-08-25 17:33:48 +0300

    add kafka listener & kafka settings

---
+++ src/main/java/com/example/bookreservationhibercompiler/kafkaListener/BookKafkaListener.java
@@ -0,0 +1,26 @@
+package com.example.bookreservationhibercompiler.kafkaListener;
+
+import com.example.bookreservationhibercompiler.dto.BookDTO;
+import com.example.bookreservationhibercompiler.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@EnableKafka
+//@Service
+public class BookKafkaListener {
+
+    @Autowired
+    private BookService bookService;
+    ObjectMapper mapper = new ObjectMapper();
+
+    @KafkaListener(topics = {"book-add-topic"})
+    public void getTopics(@RequestBody String data) throws Exception {
+        BookDTO bookDTO = mapper.readValue(data, BookDTO.class);
+        System.out.println(bookDTO);
+        String id = bookService.create(bookDTO).getId();
+        bookDTO.setId(id);
+    }
+}
