e0c9f49180e0dcaaa7fb76ead7b505237b634966
KovalevKA <kss47242@gmail.com>
2021-08-27 15:19:23 +0300

    Update BookKafkaListener.java
    Remove comments

--- src/main/java/com/example/bookreservationhibercompiler/kafkaListener/BookKafkaListener.java
+++ src/main/java/com/example/bookreservationhibercompiler/kafkaListener/BookKafkaListener.java
@@ -6,21 +6,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @EnableKafka
-//@Service
+@Service
 public class BookKafkaListener {
 
     @Autowired
     private BookService bookService;
+
     ObjectMapper mapper = new ObjectMapper();
 
     @KafkaListener(topics = {"book-add-topic"})
     public void getTopics(@RequestBody String data) throws Exception {
         BookDTO bookDTO = mapper.readValue(data, BookDTO.class);
-        System.out.println(bookDTO);
-        String id = bookService.create(bookDTO).getId();
+        Long id = bookService.create(bookDTO).getId();
         bookDTO.setId(id);
+        System.out.println(bookDTO);
+
     }
 }
